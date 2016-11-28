981e55ae5364eb3935d4f918ee62291ffa03713d
Sean Hoilett <sean@medullan.com>
2016-11-28 15:04:24 -0400

    set up to expose simple function

---
+++ EngaugeTx/AuthSvc.swift
@@ -0,0 +1,40 @@
+//
+//  AuthSvc.swift
+//  EngaugeTx
+//
+//  Created by Sean Hoilett on 11/28/16.
+//  Copyright © 2016 Medullan Platform Solutions. All rights reserved.
+//
+
+import Foundation
+
+
+/**
+ Provides authentication to the EnguageTx Platform
+ */
+public class AuthSvc {
+    
+    public init() {
+        
+    }
+    
+    /**
+     Login with username
+     - parameter username: The user's username
+     - parameter password: The user's password
+     ---
+     */
+    public func loginUserWithUsername(_ username: String, password: String) {
+        
+    }
+    
+    /**
+     Login with email address
+     - parameter email: The user's email address
+     - parameter password: The user's password
+     ---
+     */
+    public func loginUserWithEmail(_ email: String, password: String) {
+        
+    }
+}
---
+++ EngaugeTx/EngaugeTx.swift
@@ -0,0 +1,9 @@
+//
+//  EngaugeTx.swift
+//  EngaugeTx
+//
+//  Created by Sean Hoilett on 11/28/16.
+//  Copyright © 2016 Medullan Platform Solutions. All rights reserved.
+//
+
+import Foundation
