1d1cbea1e90dec58a24569e9a2a2977555c5e34c
Sean Hoilett <sean@medullan.com>
2016-11-28 12:27:38 -0400

    Initial Commit

---
+++ EngaugeTx/EngaugeTx.h
@@ -0,0 +1,19 @@
+//
+//  EngaugeTx.h
+//  EngaugeTx
+//
+//  Created by Sean Hoilett on 11/28/16.
+//  Copyright © 2016 Medullan Platform Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EngaugeTx.
+FOUNDATION_EXPORT double EngaugeTxVersionNumber;
+
+//! Project version string for EngaugeTx.
+FOUNDATION_EXPORT const unsigned char EngaugeTxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EngaugeTx/PublicHeader.h>
+
+
---
+++ EngaugeTxTests/EngaugeTxTests.swift
@@ -0,0 +1,36 @@
+//
+//  EngaugeTxTests.swift
+//  EngaugeTxTests
+//
+//  Created by Sean Hoilett on 11/28/16.
+//  Copyright © 2016 Medullan Platform Solutions. All rights reserved.
+//
+
+import XCTest
+@testable import EngaugeTx
+
+class EngaugeTxTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
