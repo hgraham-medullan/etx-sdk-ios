82ebf9c4be37f0c8a0a0d3736ffd04355f06f3f9
Sean H <sean@medullan.com>
2016-12-12 16:00:09 -0400

    Develop (#6)
    * set up to expose simple function
    
    * Pod configurations
    
    * Pod spec updates
    
    * Updates to podspec
    
    * Create README.md
    
    * Updating podspec and generating docs
    
    * Update to use callback function
    
    * code doc
    
    * Reading the config file to authenticate the application
    
    * Updated tests
    
    * Updated docs
    
    * Update README.md
    
    * Update README.md
    
    * Updates to flow
    
    * PodSpec update
    
    * Using siesta framework
    
    * Integration with API
    
    * Updated with login failure status
    Added remember me check
    
    * Documentation update
    
    * Version bump
    
    * CircleCI config
    
    * removing failing tests
    
    * Deploy script for releases
    
    * added build status
    pod deploy script
    
    * adding tag name and commit hash to doc commit
    
    * final deploy steps and config

--- EngaugeTx/UserService.swift
+++ EngaugeTx/UserService.swift
@@ -24,7 +24,7 @@ open class ETXUserService {
         self.userRepository = UserRepository()
     }
     
-    /** Login with username
+    /** Login with username.
      - parameter username: The user's username
      - parameter password: The user's password
      - parameter rememberMe: Allows for an extended user session
@@ -38,7 +38,7 @@ open class ETXUserService {
     }
     
     /**
-     Login with email address
+     Login with email address.
      - parameter email: The user's email address
      - parameter password: The user's password
      - parameter rememberMe: Allows for an extended user session
