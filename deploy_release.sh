f50b845740a1dcb4d946b3e646abb01a50f4735a
Wesley Boland <wesley@wesleyboland.com>
2018-09-09 17:06:50 +0000

    test 10

--- deploy_release.sh
+++ deploy_release.sh
@@ -26,5 +26,5 @@ set -o pipefail
 
 echo "Deploying the pod..."
 pod repo add EngaugeTxPodSpecs https://github.com/medullan/engauge-tx-pod-specs.git
-pod repo push EngaugeTxPodSpecs EngaugeTx.podspec --allow-warnings
+#pod repo push EngaugeTxPodSpecs EngaugeTx.podspec --allow-warnings
 echo "pod deploy complete."
